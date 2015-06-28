82f159b007c9890f8d0881bcd57eab11624b5ef0
Marco Wahl <marcowahlsoft@gmail.com>
2015-06-28 21:41:16 +0200

    use the 'element of' symbol for \in in org-files

--- init.el
+++ init.el
@@ -1140,6 +1140,18 @@ window."
             (prettify-symbols-mode 1)))
 ;; #+END_SRC
 
+;; *** org
+
+;; try out using the symbol for 'element of'.
+
+;; #+BEGIN_SRC emacs-lisp
+(add-hook 'org-mode-hook
+          (lambda ()
+            (setq-local prettify-symbols-alist
+                        '(("\\in" . ?∈)))
+            (prettify-symbols-mode 1)))
+;; #+END_SRC
+
 ;; ** language environment change
 
 ;; Convenient switching of the input-method and the spell-checking.
