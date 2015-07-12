9207370a7ebd8910134dff51bdce834ca9b3f3a6
Marco Wahl <marcowahlsoft@gmail.com>
2015-07-12 21:59:27 +0200

    activated dired-x

--- init.el
+++ init.el
@@ -1096,6 +1096,24 @@ window."
 (autoload 'mpages "mpages" "For writing morning pages." t nil)
 ;; #+END_SRC
 
+;; ** dired-x
+
+;; #+BEGIN_SRC emacs-lisp
+     (add-hook 'dired-load-hook
+                (lambda ()
+                  (load "dired-x")
+                  ;; Set dired-x global variables here.  For example:
+                  ;; (setq dired-guess-shell-gnutar "gtar")
+                  ;; (setq dired-x-hands-off-my-keys nil)
+                  ))
+      (add-hook 'dired-mode-hook
+                (lambda ()
+                  ;; Set dired-x buffer-local variables here.  For example:
+                  ;; (dired-omit-mode 1)
+                  ))
+;; #+END_SRC
+
+
 ;;; Rest:
 
 ;; ** Enable more emacs features
