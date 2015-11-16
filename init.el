0460877e53a315e9b2d2ba5553f2cf6872b2c2aa
Marco Wahl <marcowahlsoft@gmail.com>
2015-11-16 12:44:45 +0100

    init.el (mw-individual-keymap): prefix for C-z z
    +delete to point-min with C-u C-z z

--- init.el
+++ init.el
@@ -1748,7 +1748,7 @@ Originates from gnu.emacs.help group 2006."
     (define-key map "p" #'password-store-copy)
     (define-key map "P" #'org-toggle-pretty-entities)
     (define-key map "j" #'org-clock-goto)
-    (define-key map "z" #'mw-auxies-delete-to-point-max)
+    (define-key map "z" #'mw-auxies-delete-to-direction)
     (define-key map "k" #'key-chord-mode) ; sometimes i need turning it off and on again.
     (define-key map "c" #'calendar)
     (define-key map "q" #'bury-buffer)
