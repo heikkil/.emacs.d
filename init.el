c1fdfdb281861ebfb19f15cbf9982fcffb166a64
Marco Wahl <marcowahlsoft@gmail.com>
2015-12-02 15:34:51 +0100

    binding for entering evil mode

--- init.el
+++ init.el
@@ -1788,7 +1788,8 @@ Originates from gnu.emacs.help group 2006."
                                         ; recall: from org-files there is already C-u C-u C-c C-w.
     (define-key map "<" #'mw-screen-exchange-slurp-insert)
     (define-key map ">" #'mw-screen-exchange-write-region)
-    (define-key map "e" (lambda () "Erase org-stored-links" (interactive) (setq org-stored-links nil)))
+    (define-key map "e" #'evil-mode)
+    (define-key map "E" (lambda () "Erase org-stored-links" (interactive) (setq org-stored-links nil)))
     (define-key map "(" (lambda () (interactive) (paredit-mode)))
     (define-key map ")" (lambda () (interactive) (disable-paredit-mode)))
     (define-key map "W" #'calc-embedded-word)
