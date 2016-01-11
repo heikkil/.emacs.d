8587a0e26e4cb590eaed5211142c853e6a720f9b
Marco Wahl <marcowahlsoft@gmail.com>
2016-01-11 12:41:33 +0100

    +:section Copyright

--- init.el
+++ init.el
@@ -1,15 +1,28 @@
 ;;; init.el --- personal emacs config file
 ;; #+options: toc:2
 
-;; Copyright (c) 2015 Marco Wahl <marcowahlsoft@gmail.com>
+;;; Copyright:                                                        :noexport:
 
-;; #+TITLE: Personal Emacs Config
-;; #+AUTHOR: Marco Wahl
-;; #+EMAIL: marcowahlsoft@gmail.com
+;; Copyright (c) 2015-2016 Marco Wahl <marcowahlsoft@gmail.com>
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or (at
+;; your option) any later version.
+
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 ;;; Metahead:                                                         :noexport:
 
 ;; This section is about this file itself.
+
+;; *** Style
 
 ;; This emacs configuration file is written in lentic style of type
 ;; org/el.
