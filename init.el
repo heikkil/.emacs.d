b21b36a6b3fbd2620952ca825b784d459ab59886
Marco Wahl <marcowahlsoft@gmail.com>
2016-05-03 15:57:18 +0200

    init.el

--- init.el
+++ init.el
@@ -265,6 +265,9 @@
                      ?m ?j ?b ?k ?w ?q ?v ?x ?z))
   (setf avy-all-windows nil)
   (setf avy-all-windows-alt t) ; hint: behavior prefix arg
+  (avy-setup-default)
+  ;; [2016-05-03 Tue 15:56]: was
+  ;;(eval-after-load "isearch" '(define-key isearch-mode-map (kbd "C-'") 'avy-isearch))
   )
 ;; #+END_SRC
 
