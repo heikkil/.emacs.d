3d6ecde0abe6c060f9a726af6ec60f8f5dcec20f
Marco Wahl <marcowahlsoft@gmail.com>
2017-02-27 10:01:34 +0100

    init.el

--- init.el
+++ init.el
@@ -4,7 +4,7 @@
 
 ;;; Copyright:                                                        :noexport:
 
-;; Copyright (c) 2015-2016 Marco Wahl <marcowahlsoft@gmail.com>
+;; Copyright (c) 2015-2017 Marco Wahl <marcowahlsoft@gmail.com>
 
 ;; This program is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
